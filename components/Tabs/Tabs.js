6edb0deb5657f3888c781fb5cef8358788325131
Roger Caballero <rogercp1234@yahoo.com>
2019-04-11 14:55:41 -0700

    added more tabs

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -25,6 +25,7 @@ class TabLink {
     // Get all of the elements with the tabs-link class
     // const links;
     const links=document.querySelectorAll(".tabs-link");
+    console.log(links);
     
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
